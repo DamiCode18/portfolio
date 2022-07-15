364608ccb9161ed8e2d5f4db47523803850eece9
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-07-15 22:44:29 +0100

    added home, about and projects sections

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// pages/_document.js
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100&display=swap" rel="stylesheet"/>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
---
+++ pages/components/Homepage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Homepage = (props: Props) => {
+  return (
+    <div className="home">
+        <h1>Hi, I’m Damilare Adebayo</h1>
+        <p>I am a Software Engineer based in Lagos, Nigeria with 3 years of software development  experience.  
+            am available for Remote or Full Time jobs.
+        </p>
+    </div>
+  )
+}
+
+export default Homepage;
