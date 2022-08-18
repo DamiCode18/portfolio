cbe8420a7cd5818264106bd9d32d9ea1ec44fd01
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-08-18 08:52:14 +0100

    font-size on different screen(home)

--- pages/homepage.tsx
+++ pages/homepage.tsx
@@ -13,8 +13,8 @@ const Homepage = (props: Props) => {
     <Fragment>
       <div className="home flex mx-[10%] justify-center mt-20">
         <div className="text-center md:text-left my-auto max-w-[630px] md:mr-14">
-          <h1 className="text-5xl bolder-text font-extrabold">Hi, I'm Damilare</h1>
-          <p className="text-justify leading-loose text-lg font-bold my-10">I am a Software Engineer based in Lagos, Nigeria with 4 years of software development  experience.
+          <h1 className="text-2xl md:text-5xl bolder-text font-extrabold">Hi, I'm Damilare</h1>
+          <p className="text-justify leading-loose text-sm md:text-lg font-bold my-10">I am a Software Engineer based in Lagos, Nigeria with 4 years of software development  experience.
             I am available for Remote or Full Time jobs.
           </p>
           <div className="mt-[4rem] mb-[2rem]">
