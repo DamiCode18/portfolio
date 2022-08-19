0a57d68d9918cc5d0a4d29eb5b1564dbc90c40c8
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-08-19 01:49:31 +0100

    some more updates on about and contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, useState } from 'react'
 import axios from 'axios';
+import Head from 'next/head'
 
 const Contact = () => {
     const [formData, setFormData] = useState({
@@ -34,17 +35,21 @@ const Contact = () => {
 
     return (
         <Fragment>
-            <form onSubmit={onSubmit} className="w-[30rem] mx-auto my-6 h-[400px]">
+        <Head>
+          <title>Damicode | Contact </title>
+        </Head>
+          <h1 className="head-shadow text-center my-[4rem] mx-[10%] justify-center mt-5">Contact</h1>
+            <form onSubmit={onSubmit} className="p-6 max-w-[30rem] mx-auto my-6 h-[400px]">
                 <div className="mb-6">
-                    <input autoComplete="off" name="name" type="text" onChange={handleChange} value={formData.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your name" required />
+                    <input autoComplete="off" name="name" type="text" onChange={handleChange} value={formData.name} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#ff7b00d8] focus:border-[#ff7b00d8] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#ff7b00d8] dark:focus:border-[#ff7b00d8" placeholder="Enter your name" required />
                 </div>
                 <div className="mb-6">
-                    <input autoComplete="off" name="email" type="email" onChange={handleChange} value={formData.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter your email" required />
+                    <input autoComplete="off" name="email" type="email" onChange={handleChange} value={formData.email} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#ff7b00d8] focus:border-[#ff7b00d8] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#ff7b00d8] dark:focus:border-[#ff7b00d8]" placeholder="Enter your email" required />
                 </div>
                 <div className="mb-6">
-                    <textarea rows={5} autoComplete="off" name="message" onChange={handleChange} value={formData.message} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Shoot your message!!! I'll definitely get back to you..." required />
+                    <textarea rows={5} autoComplete="off" name="message" onChange={handleChange} value={formData.message} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#ff7b00d8] focus:border-[#ff7b00d8] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-[#ff7b00d8] dark:focus:border-[#ff7b00d8]" placeholder="Shoot your message!!! I'll definitely get back to you..." required />
                 </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                <button type="submit" className="text-white bg-[#ff7a00] hover:bg-[#ff7b00d8] focus:ring-4 focus:outline-none focus:ring-[#ff7b00d8] font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Send message!</button>
             </form>
         </Fragment>
     )
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-
+import React,{ Fragment} from 'react'
+import Head from 'next/head'
 type Props = {}
 
 const About = (props: Props) => {
   return (
+    <Fragment>
+    <Head>
+      <title>Damicode | Projects</title>
+    </Head>
     <div className="projects mx-[10%] justify-center mt-5">
-        <h1 className="head-shadow">Projects</h1>
+        <h1 className="head-shadow">My Projects</h1>
     </div>
+    </Fragment>
   )
 }
 
