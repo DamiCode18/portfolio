f8af5b6c0fcd5ab060819c267ce55ae57f38d099
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-09-11 22:46:50 +0100

    porject fetching

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { Fragment, useState } from 'react'
 import axios from 'axios';
 import Head from 'next/head'
+
+type Data = {
+    name: string
+}
 
 const Contact = () => {
     const [formData, setFormData] = useState({
@@ -8,6 +13,8 @@ const Contact = () => {
         email: '',
         text: ''
     });
+    const [status, setStatus] = useState(false)
+    const [error, setError] = useState(false)
 
     const handleChange = (evt: any) => {
         const value = evt.target.value;
@@ -20,17 +27,18 @@ const Contact = () => {
         e.preventDefault();
         axios.post(`${process.env.NEXT_PUBLIC_EMAIL_API}`, {
             formData
-        }).then((response) => {
+        }).then(() => {
             setFormData({
                 subject: '',
                 email: '',
                 text: ''
             })
-            alert(response.status === 200 && 'Your message have been sent successfully, I will definitely get back to you soon');
-        }).catch((error) => {
-            alert(error && 'Sure your details are filled in correctly?, check again!');
-        }
-        )
+            setStatus(true);
+            setTimeout(() => {
+                setStatus(false);
+            }, 5000)
+        })
+            .catch((error) => error)
     };
 
     return (
@@ -38,6 +46,22 @@ const Contact = () => {
             <Head>
                 <title>Damicode | Contact </title>
             </Head>
+            {status && <div className="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 shadow-md px-4 py-3 rounded relative" role="alert">
+                <strong className="font-bold">Your message have been sent successfully!</strong>
+                <span className="block sm:inline">I'll definitely get back to you soon</span>
+                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg onClick={() => setStatus(false)} className="fill-current h-6 w-6 text-teal-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                </span>
+            </div>
+            }
+            {error && <div className="bg-red-100 border-t-4 border-red-500 rounded-b text-red-900 shadow-md px-4 py-3 rounded relative" role="alert">
+                <strong className="font-bold">Ooops!</strong>
+                <span className="block sm:inline">Please check that everything is alright and try again.</span>
+                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg onClick={() => setError(false)} className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" /></svg>
+                </span>
+            </div>
+            }
             <h1 className="head-shadow text-center my-[4rem] mx-[10%] justify-center mt-5">Contact</h1>
             <form onSubmit={onSubmit} className="p-6 max-w-[30rem] mx-auto my-6 h-[400px]">
                 <div className="mb-6">
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,24 +1,53 @@
-import React,{ Fragment, useEffect} from 'react'
+/* eslint-disable react/no-unescaped-entities */
+import React, { Fragment, useEffect, useState } from 'react'
 import Head from 'next/head'
 import axios from 'axios'
-type Props = {}
+import Image from 'next/image'
+type Props = {
+  res: String[],
+  id: Number,
+}
 
 const Projects = (props: Props) => {
+  const [projects, setProjects] = useState<any>([]);
+
   useEffect(() => {
     axios.get(`${process.env.NEXT_PUBLIC_DATA_URL}`)
-    .then(response=>
-      console.log(response.data.data)
-    )
-    .catch(err=>console.log(err))
+      .then(res => {
+        setProjects(res.data.data)
+      })
+      .catch(err => err)
   }, [])
   return (
     <Fragment>
-    <Head>
-      <title>Damicode | Projects</title>
-    </Head>
-    <div className="projects mx-[10%] justify-center mt-5">
+      <Head>
+        <title>Damicode | Projects</title>
+      </Head>
+      <div className="projects mx-[10%] justify-center mt-5">
         <h1 className="head-shadow">My Projects</h1>
-    </div>
+        <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 gap-4 text-center">
+          {projects.map((project: any) => (
+            <div key={project.id} className="max-w-sm rounded overflow-hidden shadow-lg">
+              <Image className="w-full" src={project.attributes.images.data.attributes.url} alt="Sunset in the mountains" width="300" height="200" />
+              <div className="px-6 py-2">
+                <div className="font-bold text-xl mb-2">{project.attributes.title}</div>
+                <p className="text-gray-700 text-base">
+                  {project.attributes.description}
+                </p>
+              </div>
+              <div className="px-6 pt-4 pb-2">
+                {project.attributes.skills && project.attributes.skills.data.map((skill: any) => (
+                  <span key={skill.id} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{skill.attributes.name}</span>
+                ))}
+                {console.log(project.attributes)}
+              </div>
+
+            </div>
+          ))
+          }
+          {/* {projects} */}
+        </div>
+      </div>
     </Fragment>
   )
 }
