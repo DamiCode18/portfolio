77851040c0c57816217de24492ddb8a68099fa07
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-10-03 23:37:23 +0100

    added more projects

--- utils/projects.ts
+++ utils/projects.ts
@@ -26,12 +26,48 @@ export const projectData = [
     skills: ["CSS", "HTML", "Javascript", "React", "GIT", "bootstrap"],
   },
   {
-    id: 3,
+    id: 4,
     url: "https://canary-bdc.surge.sh/",
     title: "SEBASTIAN",
     imgUrl: "https://canary-bdc.surge.sh/static/media/Group%2050.ede006d4.svg",
     description:
       "Sebastian, BDC helps you minimize the resources required to manage your international payments.",
+    skills: ["CSS", "HTML", "Javascript", "React", "GIT", "bootstrap"],
+  },
+  {
+    id: 5,
+    url: "https://tunga.io",
+    title: "TUNGA",
+    imgUrl:
+      "https://tunga.io/wp-content/uploads/2021/06/Tunga-office-working-1024x769.png",
+    description:
+      "Tunga is a staff augmentation firm that works with African software developers. We have helped 150+ companies worldwide with IT staffing and software outsourcing. Add experienced developers to your team within days!",
+    skills: ["CSS", "HTML", "Javascript", "Web-components", "GIT"],
+  },
+  {
+    id: 6,
+    url: "https://testing.tunga.io",
+    title: "Tunga-Testing",
+    imgUrl: "https://testing.tunga.io/static/media/Body.a185494f.png",
+    description:
+      "Tunga's testing platform, for vetting developers to be added to the tunga pool, contains various kinds of tests, ranging from multiple choice questions to code challenges and even personality tests.",
+    skills: [
+      "CSS",
+      "HTML",
+      "Javascript",
+      "React",
+      "GIT",
+      "bootstrap",
+      "styled-components",
+    ],
+  },
+  {
+    id: 7,
+    url: "https://driveme.ng",
+    title: "DrivemeNG",
+    imgUrl: "https://driveme.ng/assets/img/car.png",
+    description:
+      "DriveMe.ng is a mobility technology company with a centralised platform aggregating product & service providers in the logistics and mobility space across Africa for ease of access to users.",
     skills: ["CSS", "HTML", "Javascript", "React", "GIT", "bootstrap"],
   },
 ];
