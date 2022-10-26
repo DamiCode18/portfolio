91a729b8ef9a3223844334516a90e790949bf5d4
DamiCode18 <adebayoribukunoluwa@yahoo.com>
2022-10-26 02:07:37 +0100

    cleanup footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = (props: Props) => {
   return (
     <div className="mt-[5rem]">
     <footer className='fixed border-[#474444] bg-black border-t-[1px] bottom-0 h-[60px] w-[100%] items-center justify-center inline-flex'>
-      <p>
+      <p className='text-xs'>
         Copyright {new Date().getFullYear()} &copy; Damicode
       </p>
     </footer>
