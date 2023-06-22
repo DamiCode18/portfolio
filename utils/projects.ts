2b84708b328c31b4a0defd40b0e098f1fd79f625
damicode18 <adebayoribukunoluwa@yahoo.com>
2023-06-22 19:22:47 +0100

    updated env file

--- utils/projects.ts
+++ utils/projects.ts
@@ -4,8 +4,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://bodsquare.com",
     title: "BodSquare",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668813282/Portfolio/Screenshot_2022-11-19_at_12.09.14_AM_k7ehfo.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668813282/Portfolio/Screenshot_2022-11-19_at_12.09.14_AM_k7ehfo.png`,
     description: "This is the official Bodsquare website",
     skills: [
       "CSS",
@@ -21,8 +20,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://www.sendchamp.com/",
     title: "Sendchamp",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1679333320/Portfolio/Screenshot_2023-03-20_at_6.28.23_PM_flrtaf.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1679333320/Portfolio/Screenshot_2023-03-20_at_6.28.23_PM_flrtaf.png`,
     description:
       "Sendchamp is the most seamless messaging platform for growing businesses. Send messages to your customers across WhatsApp, SMS, email, and voice - all with zero coding skills to get started.",
     skills: ["Chakra", "HTML", "Javascript", "React", "TypeScript", "Recoiljs"],
@@ -31,8 +29,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://kokua.wiki",
     title: "Kokua - Free Resources",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1672748951/Portfolio/Screenshot_2023-01-03_at_1.28.50_PM_bethua.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1672748951/Portfolio/Screenshot_2023-01-03_at_1.28.50_PM_bethua.png`,
     description:
       "A free resources application, very handy for all beginner and professioner developers",
     skills: ["Chakra", "HTML", "Javascript", "Nextjs", "TypeScript", "Strapi"],
@@ -41,8 +38,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://anonn.xyz",
     title: "Anonn - Anonymous Chat",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1672750374/Portfolio/Screenshot_2023-01-03_at_1.52.23_PM_lxtvb5.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1672750374/Portfolio/Screenshot_2023-01-03_at_1.52.23_PM_lxtvb5.png`,
     description:
       "Anonymously chat with people, they are free to say anything to you since you never get to know who you're chatting with except they tell you",
     skills: ["Tailwindcss", "HTML", "Javascript", "Nextjs", "TypeScript"],
@@ -51,8 +47,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://damicode.netlify.app",
     title: "Portfolio",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668812884/Portfolio/Screenshot_2022-11-18_at_11.59.29_PM_ndnum9.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1687457273/Screenshot_2023-06-22_at_7.07.25_PM_cjqhvr.png`,
     description:
       "Just a portfolio app, that shows all my projects, I bet you're currently viewing it",
     skills: ["CSS", "HTML", "Javascript", "Nextjs", "TypeScript"],
@@ -61,8 +56,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://dash.bodsquare.com",
     title: "BodSquare WebApp",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668813281/Portfolio/Screenshot_2022-11-19_at_12.10.17_AM_vypmpp.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668813281/Portfolio/Screenshot_2022-11-19_at_12.10.17_AM_vypmpp.png`,
     description:
       "Bodsquare allows for businesses to follow up with their customers from the moment they contact them until the end, regardless of the platform used.",
     skills: [
@@ -79,8 +73,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://cybersocafrica.com",
     title: "CybersocAfrica",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668810549/Portfolio/Screenshot_2022-11-18_at_11.28.52_PM_djdjvo.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668810549/Portfolio/Screenshot_2022-11-18_at_11.28.52_PM_djdjvo.png`,
     description: "This is the official CybersocAfrica website",
     skills: ["CSS", "HTML", "Javascript", "GIT", "bootstrap"],
   },
@@ -88,8 +81,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://cyberlabafrica.com",
     title: "Cyberlab",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668813281/Portfolio/Screenshot_2022-11-19_at_12.12.19_AM_bb2s0q.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668813281/Portfolio/Screenshot_2022-11-19_at_12.12.19_AM_bb2s0q.png`,
     description: "This is the official Cyberslab website",
     skills: ["CSS", "HTML", "Javascript", "GIT", "bootstrap"],
   },
@@ -97,8 +89,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://canary-bdc.surge.sh/",
     title: "SEBASTIAN",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668813282/Portfolio/Screenshot_2022-11-19_at_12.12.49_AM_yikkfm.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668813282/Portfolio/Screenshot_2022-11-19_at_12.12.49_AM_yikkfm.png`,
     description:
       "Sebastian, BDC helps you minimize the resources required to manage your international payments.",
     skills: ["CSS", "HTML", "Javascript", "React", "GIT", "bootstrap"],
@@ -107,8 +98,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://tunga.io",
     title: "TUNGA",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668813282/Portfolio/Screenshot_2022-11-19_at_12.13.14_AM_wiu4jz.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668813282/Portfolio/Screenshot_2022-11-19_at_12.13.14_AM_wiu4jz.png`,
     description:
       "Tunga is a staff augmentation firm that works with African software developers. We have helped 150+ companies worldwide with IT staffing and software outsourcing. Add experienced developers to your team within days!",
     skills: ["CSS", "HTML", "Javascript", "Web-components", "GIT"],
@@ -117,8 +107,7 @@ export const projectData = [
     id: uuidv4(),
     url: "https://testing.tunga.io",
     title: "Tunga-Testing",
-    imgUrl:
-      "https://res.cloudinary.com/damicode/image/upload/v1668813580/Portfolio/Screenshot_2022-11-19_at_12.19.20_AM_wftl62.png",
+    imgUrl: `${process.env.NEXT_PUBLIC_ASSETS}/v1668813580/Portfolio/Screenshot_2022-11-19_at_12.19.20_AM_wftl62.png`,
     description:
       "Tunga's testing platform, for vetting developers to be added to the tunga pool, contains various kinds of tests, ranging from multiple choice questions to code challenges and even personality tests.",
     skills: [
