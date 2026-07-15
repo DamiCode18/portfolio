8d78e48737d8c4089257c7e7ff03a81971506d6a
damicode18 <adebayoribukunoluwa@yahoo.com>
2026-07-15 14:09:19 +0100

    Refactored image handling in Projects component by removing custom loader and implementing Next.js layout feature for improved performance and simplicity.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,6 @@
 import React, { Fragment } from "react";
 import Image from "next/image";
-import { customLoader } from "../utils/imageLoader";
 import { projectData } from "../utils/projects";
-import { shimmer, toBase64 } from "../utils/Shimer";
 import Seo from "../components/Seo";
 
 const Projects = () => {
@@ -19,22 +17,17 @@ const Projects = () => {
             projectData?.map((project: any) => (
               <div
                 key={project.id}
-                className="transition ease-in-out duration-300 hover:scale-80 hover:-translate-y-1 delay-150 max-w-sm rounded overflow-hidden shadow-lg border-solid border-2 border-[#353131] py-2"
+                className="transition ease-in-out duration-300 hover:scale-80 hover:-translate-y-1 delay-150 max-w-sm rounded overflow-hidden shadow-lg border-solid border-2 border-[#353131]"
               >
-                <Image
-                  loader={customLoader}
-                  className="w-full"
-                  src={project.imgUrl}
-                  alt={project.title}
-                  width={300}
-                  placeholder="blur"
-                  blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
-                  quality={100}
-                  objectFit="cover"
-                  objectPosition="cover"
-                  height={150}
-                />
-                <div className="px-6 py-2">
+                <div className="relative w-full h-48">
+                  <Image
+                    src={project.imgUrl}
+                    alt={project.title}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+                <div className="px-6 py-2 pt-4">
                   <div className="font-bold text-xl mb-2">
                     <a
                       href={project.url}
