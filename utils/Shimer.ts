8c9b7c22b0ff68637c4b003fe299c5ad2a216fb7
damicode18 <adebayoribukunoluwa@yahoo.com>
2026-07-15 13:36:30 +0100

    Refactored toBase64 function for concise syntax in Shimer.ts

--- utils/Shimer.ts
+++ utils/Shimer.ts
@@ -12,8 +12,7 @@ export const shimmer = (w: number, h: number) => `
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
 </svg>`;
 
-export const toBase64 = (str: string) => {
+export const toBase64 = (str: string) =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
     : window.btoa(str);
-};
